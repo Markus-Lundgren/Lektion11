b54be50d6e8c5ef02181c98b3ab33a71ad25d55f
Markus-Lundgren <markus.lundgren.99@gmail.com>
2026-09-23 09:48:30 +0200

    feat: Add structure for assigment

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Lektion11;
+
+class Program
+{
+	static void Main()
+	{
+		//Exercise1(); // Hoppa över övning 1
+		//Exercise2();
+		//Exercise3();
+	}
+
+	static void Exercise1()
+	{
+		// kod för övning 1
+	}
+
+	static void Exercise2()
+	{
+		// kod för övning 2
+	}
+
+	static void Exercise3()
+	{
+		// kod för övning 3
+	}
+
+}
